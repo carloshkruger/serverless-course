e2b2096fe627856471095119f62d8fa5bf92bd03
carloshkruger <carloshkruger@gmail.com>
2023-01-23 20:13:14 -0300

    tooling docker serverless-offline localstack part3

---
+++ tooling-jest-docker-plus-part3/src/factory.js
@@ -0,0 +1,17 @@
+const AWS = require('aws-sdk')
+
+const s3Config = {
+  s3ForcePathStyle: true
+}
+const isLocal = process.env.IS_OFFLINE
+
+if (isLocal) {
+  const host = process.env.LOCALSTACK_HOST || 'localhost'
+  s3Config.endpoint = new AWS.Endpoint(`http://${host}:4566`)
+}
+
+const s3 = new AWS.S3(s3Config)
+
+module.exports = {
+  s3
+}
